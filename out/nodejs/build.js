d6cc4b690d0ac6f7d62ff363c613ee8ace7b749d
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2021-09-29 01:23:08 -0300

    publicando todos os proto files

--- out/nodejs/build.js
+++ out/nodejs/build.js
@@ -9,5 +9,5 @@ var parent = Path.dirname(Path.dirname(Path.dirname(__filename)));
 
 console.log(parent);
 
-const stdout = execSync(`grpc_tools_node_protoc --js_out=import_style=commonjs,binary:. --proto_path ${parent}/src --grpc_out=generate_package_definition:. ${parent}/src/echo.proto`);
+const stdout = execSync(`grpc_tools_node_protoc --js_out=import_style=commonjs,binary:. --proto_path ${parent}/src --grpc_out=generate_package_definition:. ${parent}/src/*.proto`);
 console.log(`STDOUT = ${JSON.stringify(stdout)}`);
