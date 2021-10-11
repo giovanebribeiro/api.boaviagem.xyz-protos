7e285bfe97fbac3a3f81f6c139771806cbe458c2
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2021-10-11 01:18:41 -0300

    adicionando script release.sh e ajustando documentação e build automático

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ Contém os arquivos [proto](https://developers.google.com/protocol-buffers/docs/
 Este projeto possui deploy automático por meio do Github Actions, e no momento, estes arquivos geram bibliotecas para comunicação por meio do protocolo gRPC nas seguintes linguagens:
 
 * [C#](https://github.com/giovanebribeiro/api.boaviagem.xyz-contracts/packages/991213)
-* [Javascript (Node.js)]()
+* [Javascript (Node.js)](https://github.com/giovanebribeiro/api.boaviagem.xyz-contracts/packages/1015252)
 
 ## Geração dos arquivos para compilação
 
 Para a geração dos arquivos para algumas linguagens é necessário o [compilador](https://grpc.io/docs/protoc-installation/) para arquivos protobuf.
+
+## Versionamento
+
+Para gerar o versionamento, rode o script release.sh. Será feita a geração de versão e criação do release utilizando a API do Github (OBS: a variável GITHUB_TOKEN precisa estar setada).
--- release.sh
+++ release.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
-if [ "$1" != "" ]; then
-	VERSION=$1
-else
-	echo "Informe uma versão. Ex: $0 1.0.0"
+usage(){
+	echo "Version: $(git describe --tags --abbrev=0)"
+	echo "Usage: $0 [options]"
+	echo " h _______________ print this help"
+	echo " v tag_name ______ create a tag git using tag_name. All version files from output libraries will be updated." 
+	echo " r _______________ create a release for github API"
+	echo " t _______________ skip the git tag creation"
+	echo
+	echo "PS: You must have the GITHUB_TOKEN env set for the 'r' option works"
+}
+
+if [ $# -eq 0 ];then
+	usage
 	exit 1
 fi
 
-VERSION=$1
+while getopts "h:v:rt" OP; do
+	case "${OP}" in
+		h)
+			usage
+			exit 0
+			;;
+		v)
+			VERSION=$OPTARG
+			;;
+		r)
+			RELEASE=1
+			;;
+		t) 
+			NO_TAG_GIT=1
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
 
 echo "#########"
 echo "### Atualizando descrição de pacotes com a versão $VERSION"
@@ -24,4 +53,17 @@ cd out/nodejs
 npm version $VERSION --no-git-tag-version -m "atualização de versão - pacote nodejs" 
 cd $PWD
 echo "* Node.JS (OK)"
-git tag -a $VERSION -m "release version $VERSION" 
+
+if [ "$NO_TAG_GIT" != "1" ]; then
+	git tag -a v$VERSION -m "release version $VERSION" 
+	git push --tags
+fi
+
+if [ "$RELEASE" == "1" ]; then
+	echo "* Creating Github release"
+	API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
+	OWNER=giovanebribeiro
+	REPO=api.boaviagem.xyz-contracts
+	curl -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.v3+json' --data "$API_JSON" https://api.github.com/repos/$OWNER/$REPO/releases
+	echo "* Creating Github release (OK)"
+fi
