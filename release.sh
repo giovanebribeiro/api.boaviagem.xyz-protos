19a8e5ddcadd31496c40f16115434dd192a87e5a
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2021-10-10 23:13:36 -0300

    adicionando release.sh - tag 0.1.1

--- release.sh
+++ release.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+if [ "$1" != "" ]; then
+	VERSION=$1
+else
+	echo "Informe uma versão. Ex: $0 1.0.0"
+	exit 1
+fi
+
+VERSION=$1
+
+echo "#########"
+echo "### Atualizando descrição de pacotes com a versão $VERSION"
+echo "#########"
+echo 
+echo 
+echo 
+sed -i -e "s/\<Version\>.*\<\/Version\>/<Version>$VERSION<\/Version>/g" out/csharp/*.csproj
+git add out/csharp/*.csproj
+git commit -m "atualização de versão - pacote C#"
+echo "* C# (OK)"
+BASEDIR=$PWD
+cd out/nodejs
+npm version $VERSION --no-git-tag-version -m "atualização de versão - pacote nodejs" 
+cd $PWD
+echo "* Node.JS (OK)"
+git tag -a $VERSION -m "release version $VERSION" 
